3572ec53172b421ee15da3284e755a7c436a4014
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-09-25 21:58:20 +0200

    Otros ejercicios

---
+++ ejemploFuncionesAritmeticasBasicas.py
@@ -0,0 +1,20 @@
+def sumar(a,b):
+    return ("La suma es: " + str((a + b)))
+    
+def restar(a,b):
+    return ("La resta es: " +  str((a - b)))
+
+def multiplicar(a,b):
+    return ("La multiplicación es: " +  str((a * b)))
+    
+    
+def dividir(a,b):
+    return ("La división es: " +  str((a / b)))
+
+
+print(sumar(5,5))
+print(restar(5,5))
+print(multiplicar(5,5))
+print(dividir(5,5))
+
+
--- ejemplo_Tutorial.py
+++ ejemplo_Tutorial.py
@@ -1,15 +1,7 @@
 
 
 """
-#crea un rango de numeros y ordenalos en una lista de 
-# pares e impares
-#7 y 10 = [7,8,9,10]
-
-def rango_de_numeros(a,b):
-    rango_final=[]
-    for numero in range(a,b+1):
-        rango_final.append(numero)
-    return rango_final
+return ("La división es: " +  str((a / b)))
 
 
 """
