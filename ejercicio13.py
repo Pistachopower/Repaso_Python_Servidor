e7f3768cbd752b082f1a401abd84d276f583230a
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-09-25 20:15:45 +0200

    Se agregan dos ejercicios de POO básicos

---
+++ ejercicio13.py
@@ -0,0 +1,26 @@
+"""
+Crea una clase llamada Estudiante con atributos nombre, edad y curso. 
+Crea varios objetos de tipo Estudiante y almacénalos en una lista. 
+Luego, itera sobre la lista e imprime la información de cada estudiante.  
+"""
+class Estudiante:
+    def __init__(self, nombre, edad, curso):
+        self.nombre = nombre
+        self.edad= edad
+        self.curso= curso
+
+    def __str__(self):
+        return f"Nombre: {self.nombre}, Edad: {self.edad}, Curso: {self.curso}"
+
+
+#creamos los objetos
+estudiante1= Estudiante("Nelson", 31, "daw")
+estudiante2= Estudiante("Pedro", 28, "daw")
+estudiante3= Estudiante("Margarito", 25, "daw")
+
+#almacenamos los los estudiantes en una lista
+cursoDaw= [estudiante1, estudiante2, estudiante3]
+
+#imprimimos la informacion de los estudiante
+for estudiante in cursoDaw:
+    print(estudiante) 
---
+++ ejercicio14.py
@@ -0,0 +1,36 @@
+"""
+Crea una clase llamada CuentaBancaria con atributos titular y saldo. 
+Agrega métodos para depositar y retirar dinero de la cuenta. 
+"""
+#definimos clase CuentaBancaria
+class CuentaBancaria:
+   #creamos el constructor
+    def __init__(self, titular, saldo):
+       #definimos los atributos del constructor
+       self.titular= titular
+       self.saldo= saldo
+      
+    def __str__(self):
+        return f"Titular:  {self.titular}, Saldo: {self.saldo} €"
+
+
+    def depositar(self, monto):
+        self.saldo= self.saldo + monto
+
+    def retiro(self, montoRetiro):
+        self.saldo= self.saldo - montoRetiro
+        
+        
+#instanciamos los objetos
+cuenta1= CuentaBancaria("Juan Gomez", 150)
+
+print("Saldo inicial")
+print(cuenta1)
+
+print("Saldo con depósito")
+cuenta1.depositar(50)
+print(cuenta1)
+
+print("Saldo con retiro")
+cuenta1.retiro(50)
+print(cuenta1)
